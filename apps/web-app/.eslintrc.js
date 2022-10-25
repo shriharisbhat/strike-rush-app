f9ca7584c3c63175cdacbdc6a37ee610f9d5bc9b
sriharisbhat <srihari.bhat@robosoftin.com>
2022-10-25 21:48:11 +0530

    Create rig folder with web profile, added heft and webpack configs setup react app with webpack added react, react-dom to dep and 4 babel packages, @types/heft-jest, @types/react, @types/react-dom, @types/webpack-env to devDep

---
+++ apps/web-app/.eslintrc.js
@@ -0,0 +1,13 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require("@rushstack/eslint-config/patch/modern-module-resolution");
+
+module.exports = {
+  extends: [
+    "@rushstack/eslint-config/profile/web-app",
+    "@rushstack/eslint-config/mixins/react",
+  ],
+  parserOptions: { tsconfigRootDir: __dirname },
+};
+
+// Note: If your project uses the React framework, you should also extend from the "@rushstack/eslint-config/mixins/react" mixin.
+// See the documentation for details about @rushstack/eslint-config "profiles" and "mixins".
---
+++ apps/web-app/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+
+const rootDiv: HTMLElement = document.getElementById("root") as HTMLElement;
+ReactDOM.render(<App />, rootDiv);
