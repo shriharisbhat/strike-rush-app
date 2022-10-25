942f280fe75b0dd3921e95c69a87adf4cf93a343
sriharisbhat <srihari.bhat@robosoftin.com>
2022-10-25 22:37:57 +0530

    add eslint and typescript packages to dev dep this fixed the issue where eslint plugin was not deteting and displaying the error added some rules to .eslint.js file

--- apps/web-app/.eslintrc.js
+++ apps/web-app/.eslintrc.js
@@ -4,9 +4,45 @@ require("@rushstack/eslint-config/patch/modern-module-resolution");
 module.exports = {
   extends: [
     "@rushstack/eslint-config/profile/web-app",
+    "@rushstack/eslint-config/mixins/tsdoc",
     "@rushstack/eslint-config/mixins/react",
   ],
   parserOptions: { tsconfigRootDir: __dirname },
+
+  settings: {
+    react: {
+      version: 16,
+    },
+  },
+
+  overrides: [
+    {
+      // Extend the base override for TypeScript
+      files: ["*.ts", "*.tsx"],
+    },
+    {
+      // Extend the base override for TSX Files
+      files: ["*.tsx"],
+
+      rules: {
+        "no-console": ["error", { allow: ["warn", "error", "info"] }],
+        // Allow render functions to omit return type
+        "@typescript-eslint/explicit-function-return-type": "off",
+
+        // Allow StyleSheets to be defined at the end of the file
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            vars: "all",
+            args: "after-used",
+            ignoreRestSiblings: false,
+            argsIgnorePattern: "^_",
+          },
+        ],
+      },
+    },
+  ],
 };
 
 // Note: If your project uses the React framework, you should also extend from the "@rushstack/eslint-config/mixins/react" mixin.
