715a66584bd506d3b7a442ff9848877f27a76a99
sriharisbhat <srihari.bhat@robosoftin.com>
2022-10-28 17:59:15 +0530

    format file

--- apps/web-app/src/index.tsx
+++ apps/web-app/src/index.tsx
@@ -2,21 +2,20 @@ import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 declare global {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    interface Window {
-      __meta: {
-        VERSION: string;
-        BUILD_MODE: string;
-      };
-    }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  interface Window {
+    __meta: {
+      VERSION: string;
+      BUILD_MODE: string;
+    };
   }
-  
- 
-  window.__meta = {
-    VERSION: __LAST_COMMIT__,
-    BUILD_MODE: __BUILD_MODE__
-  };
+}
+
+window.__meta = {
+  VERSION: __LAST_COMMIT__,
+  BUILD_MODE: __BUILD_MODE__,
+};
 
 const container: HTMLElement = document.getElementById("root") as HTMLElement;
-const root:ReactDOM.Root = ReactDOM.createRoot(container);
-root.render(<App/>);
+const root: ReactDOM.Root = ReactDOM.createRoot(container);
+root.render(<App />);
