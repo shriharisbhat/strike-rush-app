69c5dda4088461e84680bd68310f94f125a5d31e
sriharisbhat <srihari.bhat@robosoftin.com>
2022-11-04 18:59:54 +0530

    fix some issues with eslint and typescrit peerDep and devDep

--- apps/web-app/.eslintrc.js
+++ apps/web-app/.eslintrc.js
@@ -1,7 +1,9 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
 require('@strike/eslint-config/patch/modern-module-resolution');
 
+// eslint-disable-next-line no-undef
 module.exports = {
   extends: ['@strike/eslint-config/profile/web-common'],
+  // eslint-disable-next-line no-undef
   parserOptions: { tsconfigRootDir: __dirname }
 };
