bfe3aabd533704190646217e51dff9a17f878637
sriharisbhat <srihari.bhat@robosoftin.com>
2022-11-12 17:15:59 +0530

    update eslint file

--- libraries/web-ui-kit/.eslintrc.js
+++ libraries/web-ui-kit/.eslintrc.js
@@ -2,5 +2,17 @@ require('@strike/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
   extends: ['@strike/eslint-config/profile/web-app'],
-  parserOptions: { tsconfigRootDir: __dirname }
+  parserOptions: { tsconfigRootDir: __dirname },
+  overrides: [
+    {
+      // Extend the base override for TypeScript
+      files: ['*.ts', '*.tsx'],
+
+      rules: {
+        'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+        // Allow render functions to omit return type
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    }
+  ]
 };
